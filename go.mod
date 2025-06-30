e96784a8bceabbe536ee85f9d04b9906a8624b73
nathan-joslin <joslinn1@udayton.edu>
2025-06-30 18:24:36 -0400

    add WIP data model

--- go.mod
+++ go.mod
@@ -35,6 +35,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
---
+++ pkg/oci/types.go
@@ -0,0 +1,52 @@
+package oci
+
+import (
+	"github.com/opencontainers/go-digest"
+)
+
+const (
+	// ArtifactTypeGitManifest is the artifact type for an Git manifest.
+	ArtifactTypeGitManifest = "application/vnd.act3-ai.git.repo.v1+json"
+
+	// ArtifactTypeLFSManifest is the artifact type for an Git LFS manifest.
+	ArtifactTypeLFSManifest = "application/vnd.act3-ai.git-lfs.repo.v1+json"
+
+	// MediaTypeGitConfig is the media type for a Git config.
+	MediaTypeGitConfig = "application/vnd.act3-ai.git.config.v1+json"
+
+	// MediaTypeLFSConfig is the media type for a Git LFS config.
+	MediaTypeLFSConfig = "application/vnd.act3-ai.git-lfs.config.v1+json"
+
+	// MediaTypePackLayer is the media type for a Git packfile stored as an OCI layer.
+	MediaTypePackLayer = "application/vnd.act3-ai.git.pack.v1"
+
+	// MediaTypeLFSLayer is the media type used for Git LFS layers.
+	MediaTypeLFSLayer = "application/vnd.act3-ai.git-lfs.object.v1"
+
+	// AnnotationGitRemoteOCIVersion is the key for the annotation to denote the git-remote-oci version used during the most recent operation.
+	AnnotationGitRemoteOCIVersion = "vnd.act3-ai.git-remote-oci.version"
+)
+
+// Commit represents a Git commit object identifier (OID), a SHA-1 hash.
+type Commit string
+
+// ConfigGit is an OCI manifest config, containing information about a Git repository's references.
+type ConfigGit struct {
+	// Refs map Git references to commit OID and layer digest pairs.
+	Refs map[string]ReferenceInfo `json:"refs"`
+}
+
+// ConfigLFS is an OCI manifest config, containing information about which commits an LFS file is associated with.
+type ConfigLFS struct {
+	// Refs map Git references to layers containing LFS files.
+	Refs map[string][]string `json:"commits"`
+}
+
+// ReferenceInfo holds informations about Git references stored in bundle layers.
+type ReferenceInfo struct {
+	// Commit pointed to by a reference
+	Commit Commit `json:"commit"`
+
+	// OCI layer, the packfile containing Commit
+	Layer digest.Digest `json:"layer"`
+}
